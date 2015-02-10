40fbdc86daed1d91d80b4321847e9929458754f3
Ramona Harrison <ramona.e.harrison@gmail.com>
2015-02-10 12:38:18 -0500

    added gaussian scale

--- 00 - intro/I5-gaussian-walker.rb
+++ 00 - intro/I5-gaussian-walker.rb
@@ -1,3 +1,4 @@
+import java.util.Random
 
 def setup
   size 900, 900
@@ -6,12 +7,12 @@ def setup
   no_fill
   stroke 1
 
-  #Initializes a new walker
+  @generator = Random.new
   @w = Walker.new(width, height)
 
 end
 
-# The code inside of draw is drawn over and over
+
 def draw
   @w.walk
   @w.display
@@ -20,30 +21,29 @@ end
 
 class Walker
 
-  # New walkers are created in the center of the canvas
+
   def initialize(width, height)
     @x = width/2
     @y = height/2
   end
 
-  # Walker displays itself as a black dot
+
   def display
     stroke(rand(255), rand(255), rand(255))
     ellipse(@x, @y, 1, 1);
     point(@x, @y)
   end
 
-  # Directs the walker to take a random step in any of 8 possible directions
-  # or stay still
-  def walk
-    choicex = (-1..1).to_a.sample
-    choicey = (-1..1).to_a.sample
-    sd = width/8
-    mean = height/2
 
-    x = (@generator.nextGaussian.to_f * sd) + mean
-    y = (@generator.nextGaussian.to_f * sd) + mean
-    
+  def walk
+    sd = 0.5
+    mean = 2
+    scale = (@generator.nextGaussian.to_f * sd) + mean
+
+    choicex = (-1..1).to_a.sample * scale
+    choicey = (-1..1).to_a.sample * scale
+
+
     @x += choicex
     @y += choicey
   end
